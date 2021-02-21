010a4f97be673ca79ee6dcdab6f846a5476bcf1e
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-21 11:07:38 +0000

    Initial commit - codeCademy Number Guesser PHP logic project.

---
+++ app.php
@@ -0,0 +1,4 @@
+<!-- In this project, you’ll create a number guessing game. Your program will generate a random number between 1 and 10. You’ll run the game 10 times and tell the user some information about their guessing abilities. -->
+
+<?php 
+  
